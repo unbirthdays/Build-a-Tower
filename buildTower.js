5b148a12b4361ceabb31897ba09989a6c7dd3c4c
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-27 20:46:00 -0500

    Initial commit

---
+++ buildTower.js
@@ -0,0 +1,37 @@
+/*
+Write a function that takes in a number. It should return a “tower” of *. The tower will be an array of strings.
+
+The number passed in tells you how many floors the tower should have.
+
+The first floor should have 1 block, the 2nd should have 3 blocks, the 3rd should have 5 blocks, and so on.
+
+Each block is represented by a *.
+
+For example, a tower of 3 floors looks like this (note the spacing):
+
+[
+    '  *  ',
+    ' *** ',
+    '*****'
+]
+and a tower of 6 floors looks like this:
+
+[
+    '     *     ',
+    '    ***    ',
+    '   *****   ',
+    '  *******  ',
+    ' ********* ',
+    '***********'
+]
+*/
+
+function towerBuilder(num) {
+    array = [];
+
+
+}
+
+
+console.log(tower(1));
+console.log(tower(6));
